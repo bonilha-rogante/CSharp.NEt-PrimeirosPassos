91ad5a4158b718b4d1df799cb23b2ebea7a18537
Guilherme Bonilha <bonilha.rogante@gmail.com>
2023-01-03 23:17:37 -0300

    Exercício

---
+++ Matrizes/ExFix/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExFix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de Linhas da matriz: ");
+            int l = int.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade de colunas da matriz: ");
+            int c = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[l, c];
+
+            for (int i = 0; i < l; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                for (int j = 0; j < c; j++)
+                {
+                    mat[i, j] = int.Parse(valores[j]);
+                }
+            }
+
+            Console.Write("Digite o número que deseja analisar: ");
+            int numero = int.Parse(Console.ReadLine());
+        }
+
+
+    }
+}
---
+++ Matrizes/ExRefazendo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExRefazendo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o tamanho da matriz: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string[] valores = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(valores[j]);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Diagonal Principal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(mat[i, i] + ' ');
+            }
+
+            Console.WriteLine();
+            int contador = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        contador++;
+                    }
+                }
+            }
+            Console.Write(contador);
+        }
+    }
+}
