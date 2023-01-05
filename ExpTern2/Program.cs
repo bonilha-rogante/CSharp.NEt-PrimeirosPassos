486e03710db64f3638b57730f47da8327921a13f
Guilherme Bonilha <bonilha.rogante@gmail.com>
2023-01-05 15:52:16 -0300

    ternária

---
+++ ExpTern2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExTern2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+
+            Console.Write(desconto);
+
+        }
+    }
+}
